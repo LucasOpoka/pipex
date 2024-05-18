81d092cfd55a811fd5abe690c13820b69e45e7d2
Monika Leszkowicz <momina@87-92-66-188.bb.dnainternet.fi>
2024-05-18 15:42:42 +0300

    Updated libft to fix ft_printf issues

--- sources/libft/ft_printf.c
+++ sources/libft/ft_printf.c
@@ -6,29 +6,29 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 18:27:13 by lopoka            #+#    #+#             */
-/*   Updated: 2024/05/17 10:52:59 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/05/17 09:55:16 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static inline void	ft_prnt_f(t_printf *stc, char f, va_list arg)
+static inline void	ft_prnt_f(t_printf *stc, char f, va_list *arg)
 {
 	if (f == 'c')
-		ft_prnt_c(stc, va_arg(arg, int));
+		ft_prnt_c(stc, va_arg(*arg, int));
 	if (f == 's')
-		ft_prnt_s(stc, va_arg(arg, char *));
+		ft_prnt_s(stc, va_arg(*arg, char *));
 	if (f == 'p')
-		ft_prnt_p(stc, va_arg(arg, unsigned long long));
+		ft_prnt_p(stc, va_arg(*arg, unsigned long long));
 	if (f == 'd')
-		ft_prnt_d(stc, va_arg(arg, int));
+		ft_prnt_d(stc, va_arg(*arg, int));
 	if (f == 'i')
-		ft_prnt_d(stc, va_arg(arg, int));
+		ft_prnt_d(stc, va_arg(*arg, int));
 	if (f == 'u')
-		ft_prnt_u(stc, va_arg(arg, unsigned int));
+		ft_prnt_u(stc, va_arg(*arg, unsigned int));
 	if (f == 'x')
-		ft_prnt_lx(stc, va_arg(arg, unsigned int));
+		ft_prnt_lx(stc, va_arg(*arg, unsigned int));
 	if (f == 'X')
-		ft_prnt_ux(stc, va_arg(arg, unsigned int));
+		ft_prnt_ux(stc, va_arg(*arg, unsigned int));
 	if (f == '%')
 		ft_prnt_c(stc, '%');
 	stc->i += 1;
@@ -59,15 +59,15 @@ int	ft_printf(const char *s, ...)
 	{
 		if (s[stc.i] == '%')
 		{
-			ft_prnt_f(&stc, s[stc.i + 1], arg);
-			if (!s[stc.i])
+			if (!s[stc.i + 1])
 				break ;
+			ft_prnt_f(&stc, s[stc.i + 1], &arg);
 		}
 		else
 			ft_prnt_c(&stc, s[stc.i]);
 		if (stc.err)
 			return (-1);
-		stc.i++;
+		stc.i += 1;
 	}
 	va_end(arg);
 	stc.i = write(1, stc.res, stc.index);
