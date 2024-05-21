56be62351c261f520cb12944a6370d37ce818430
lopoka <lopoka@student.hive.fi>
2024-05-21 17:36:19 +0300

    Modified function which handles opening files

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:34:58 by lopoka            #+#    #+#             */
-/*   Updated: 2024/05/21 17:26:55 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/05/21 17:35:12 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/pipex.h"
@@ -35,17 +35,17 @@ static inline char	*get_shell(char **env)
 }
 */
 
-static inline void	ft_open_in_out(t_pipex *stc, char **av)
+static inline void	ft_open_in_out(t_pipex *stc)
 {
-	stc->fd_in = open(av[1], O_RDONLY);
-	stc->fd_out = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	stc->fd_in = open(stc->av[1], O_RDONLY);
+	stc->fd_out = open(stc->av[stc->ac - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (stc->fd_in == -1)
-		ft_printf_fd(2, "%s: %s: %s\n", "pipex", av[1], strerror(errno));
+		ft_printf_fd(2, "%s: %s: %s\n", "pipex", stc->av[1], strerror(errno));
 	if (stc->fd_out == -1)
 	{
 		if (stc->fd_in != -1)
 			close(stc->fd_in);
-		ft_printf_fd(2, "%s: %s: %s\n", "pipex", av[4], strerror(errno));
+		ft_printf_fd(2, "%s: %s: %s\n", "pipex", stc->av[stc->ac - 1], strerror(errno));
 		exit (1);
 	}
 }
@@ -74,7 +74,7 @@ static inline void	ft_wait_and_exit(t_pipex *stc)
 }
 */
 
-void	ft_ret_err(t_pipex *stc, int i)
+static inline void	ft_ret_err(t_pipex *stc, int i)
 {
 	while (i >= 0)
 	{
@@ -98,9 +98,9 @@ int	main(int ac, char **av, char **env)
 	stc.ac = ac;
 	stc.av = av;
 	stc.env = env;
-	if (ac != 5)
+	if (ac < 5)
 		exit (1);
-	ft_open_in_out(&stc, av);
+	ft_open_in_out(&stc);
 	i = ac - 3;
 	stc.errarr = (int *) malloc(i * sizeof(int));
 	if (!stc.errarr)
